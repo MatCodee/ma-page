c926110d44a9ee0c9eedd1fb4c24a82f52a73701
Matish1 <matias.altamiranove@gmail.com>
2023-01-14 17:09:16 -0300

    data

--- .nuxt/dist/server/server.js
+++ .nuxt/dist/server/server.js
@@ -1305,7 +1305,7 @@ async function setContext(app, context) {
   // If context not defined, create it
   if (!app.context) {
     app.context = {
-      isStatic: false,
+      isStatic: true,
       isDev: false,
       isHMR: false,
       app,
@@ -2421,6 +2421,9 @@ const layouts = {
     },
     isFetching() {
       return this.nbFetching > 0;
+    },
+    isPreview() {
+      return Boolean(this.$options.previewData);
     }
   },
   methods: {
